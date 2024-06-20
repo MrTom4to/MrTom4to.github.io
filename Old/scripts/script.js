4b97dfe3ea6f008ef9565d49957299e4aeda559d
Quentin DETRE <q-detre@sfi.fr>
2024-06-20 17:09:41 +0200

    commit4

---
+++ Old/scripts/script.js
@@ -0,0 +1,42 @@
+function affscore (scoref, nomProp) {
+  let spanScore = document.querySelector(".zoneScore span")
+  let affichageScore = `${score} / ${nomProp}`
+  console.log("Votre score est de " + scoref + " sur " + nomProp)
+}
+
+function Choix () {
+  let answer = prompt("Vouler vous la listes des mots ou des phrases ?")
+  while (answer !== "mots" && answer !== "phrases"){
+      
+    answer = prompt("Vouler vous la listes des mots ou des phrases ?")
+    }
+    return answer
+}
+
+function LancBoucJeu (ListesProp) {
+  for (i = 0; i < ListesProp.length; i++){
+Valider
+
+    let MotUtilisateur = prompt("Entrez le mot: " + ListesProp [i])
+ 
+  if (ListesProp [i] === MotUtilisateur) {
+ 
+   score++ }}
+   return score
+}
+function LancJeu () {
+let answer = Choix ()
+let NumMotProp = 0
+  switch (answer) {
+    case "mots": 
+      LancBoucJeu (listeMots)
+      NumMotProp = listeMots.length
+      break
+    case "phrases":
+      LancBoucJeu (listePhrases)
+      NumMotProp = listePhrases.length
+      break
+  }
+
+affscore (score, NumMotProp)
+}
---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+lancerJeu()
+
+let reponse = document.getElementById("inputEcriture");
+console.log(reponse)
+let boutton = document.getElementById("btnValiderMot");
+console.log(boutton)
+
+
+let baliseZonePropositionSpan = document.querySelector(".zoneProposition");
+console.log(baliseZonePropositionSpan)
+let baliseZoneScoreSpan = document.querySelector(".zoneScore span");
+console.log(baliseZoneScoreSpan)
+
+let listeInputRadio = document.querySelectorAll(".optionSource input");
+console.log(listeInputRadio)
